428562642abb617ac99ce6d32f0bd4d49c6919c3
louy999 <louyhany999@gmail.com>
2025-04-25 17:45:22 +0300

    some edit

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -95,7 +95,7 @@ const ClientsPage = () => {
           </h2>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 place-items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center">
           {certificates.map((cert, index) => (
             <div
               key={index}
--- src/app/components/home/baner.tsx
+++ src/app/components/home/baner.tsx
@@ -2,20 +2,20 @@ import React from "react";
 
 const Baner = () => {
   return (
-    <div className="h-[30vh] lg:h-[70vh] w-[100vw] p-5 left-2/4 lg:left-1/4 -translate-x-2/4 z-40 absolute flex items-center justify-center overflow-x-hidden">
-      <div className="text-center space-y-4 bg-[#5A9BD5]/80 rounded-md">
-        <h1 className="text-base md:text-xl lg:text-3xl font-bold text-white drop-shadow-md">
+    <div className="h-fit top-3/12 w-[40vh] md:w-[50vw] lg:w-[30vw] rounded-md  left-2/4 lg:left-1/4 -translate-x-2/4 z-40 absolute shadow-lg border border-white/20">
+      <div className="text-center space-y-4 p-4 bg-[#5A9BD5]/90 rounded-md w-full">
+        <h1 className="text-xs md:text-xl lg:text-3xl font-bold text-white drop-shadow-md">
           Owner & Founder: <span className="text-black">Eng. Magdy Riad</span>
         </h1>
-        <h2 className="text-base md:text-xl lg:text-2xl font-semibold text-white drop-shadow-md">
+        <h2 className="text-xs md:text-xl lg:text-2xl font-semibold text-white drop-shadow-md">
           Deputy General Manager:{" "}
           <span className="text-black">Dr.Sherif Magdy Riad</span>
         </h2>
-        <h3 className="text-base md:text-xl lg:text-2xl font-semibold text-white drop-shadow-md">
+        <h3 className="text-xs md:text-xl lg:text-2xl font-semibold text-white drop-shadow-md">
           Project Manager:{" "}
           <span className="text-black">Eng. Mosaad Abd ElAtty</span>
         </h3>
-        <h3 className="text-base md:text-xl lg:text-2xl font-semibold drop-shadow-md  uppercase ">
+        <h3 className="text-xl md:text-2xl lg:text-4xl font-bold drop-shadow-md  uppercase text-yellow-300">
           <span className="text-white">&quot;Quality</span>
           <span className="text-white">...for better buildings&quot;</span>
         </h3>
